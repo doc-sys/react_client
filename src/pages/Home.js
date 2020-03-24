c4ecd8f7be096908fc9bf4d9d7362933ba68b8e2
Felix Wieland <mail@felixwie.com>
2020-03-24 19:47:00 +0100

    written to load and siplay docs in table

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import { connect } from "react-redux";
+
+import { Box, Button, DataTable, Text, Heading, Tabs, Tab } from "grommet";
+import { DocumentLocked } from "grommet-icons";
 
 import { documentActions } from "../redux/_actions/document.actions";
 
-export default class Home extends Component {
+class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -10,7 +16,76 @@ export default class Home extends Component {
     };
   }
 
+  componentDidMount() {
+    this.props.dispatch(documentActions.getOwn());
+    this.props.dispatch(documentActions.getShared());
+  }
+
   render() {
-    return <p>Home</p>;
+    const { docs } = this.props;
+    return (
+      <Box>
+        <Heading>Documents</Heading>
+        <Tabs>
+          <Tab title="Own Documents" as={Button}>
+            <DataTable
+              columns={[
+                {
+                  property: "title",
+                  header: <Text>Title</Text>
+                },
+                {
+                  property: "fileId",
+                  header: <Text>FileID</Text>,
+                  primary: true,
+                  render: data => (
+                    <Link to={"/view/" + data.fileId}>{data.fileId}</Link>
+                  )
+                },
+                {
+                  property: "isLocked",
+                  header: null,
+                  render: data => (
+                    <Box>{data.isLocked ? <DocumentLocked /> : null}</Box>
+                  )
+                }
+              ]}
+              data={docs.ownDocuments}
+            />
+          </Tab>
+          <Tab title="Shared Documents" as={Button}>
+            <DataTable
+              columns={[
+                {
+                  property: "title",
+                  header: <Text>Title</Text>
+                },
+                {
+                  property: "fileId",
+                  header: <Text>FileID</Text>,
+                  primary: true
+                },
+                {
+                  property: "isLocked",
+                  header: null,
+                  render: data => (
+                    <Box>{data.isLocked ? <DocumentLocked /> : null}</Box>
+                  )
+                }
+              ]}
+              data={docs.sharedDocuments}
+            />
+          </Tab>
+        </Tabs>
+      </Box>
+    );
   }
 }
+
+function mapState(state) {
+  const { docs } = state;
+  return { docs };
+}
+
+const connectedHomePage = connect(mapState)(Home);
+export { connectedHomePage as Home };
