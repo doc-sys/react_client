e712b0f7dcf81d288f12c38b496ed730030b0511
Felix Wieland <mail@felixwie.com>
2020-03-24 19:47:54 +0100

    fixed error with inproper token load

--- src/redux/_helpers/authHeader.js
+++ src/redux/_helpers/authHeader.js
@@ -1,8 +1,9 @@
 export function authHeader(rest) {
   let user = JSON.parse(localStorage.getItem("user"));
+  let token = localStorage.getItem("token");
 
-  if (user && user.token) {
-    return { Authorization: "Bearer " + user.token, ...rest };
+  if (user && token) {
+    return { Authorization: "Bearer " + token, ...rest };
   } else {
     return {};
   }
