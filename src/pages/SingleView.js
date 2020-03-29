d6e191d5189a93bf506c311262a4c879d63960ee
Felix Wieland <mail@felixwie.com>
2020-03-30 00:00:17 +0200

    styled and added menu

--- src/pages/SingleView.js
+++ src/pages/SingleView.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 import { connect } from "react-redux";
 
-import { Box, Button, Text, Menu, Heading } from "grommet";
+import {
+  Box,
+  Button,
+  Text,
+  Menu,
+  Heading,
+  Grid,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell
+} from "grommet";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import Moment from "react-moment";
+import ClickToEdit from "../components/ClickToEdit";
+
+import { Download, Share, Trash, Upload } from "grommet-icons";
 
 import { documentActions } from "../redux/_actions/document.actions";
 
@@ -13,8 +27,10 @@ export default class SingleView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: ""
+      title: "Test"
     };
+
+    this.deleteFile = this.deleteFile.bind(this);
   }
 
   componentDidMount() {
@@ -25,6 +41,11 @@ export default class SingleView extends Component {
 
   componentWillUnmount() {
     this.props.dispatch(documentActions.clearSingle());
+  }
+
+  deleteFile() {
+    this.props.dispatch(documentActions.delete(this.props.match.params.fileid));
+    this.props.history.push("/");
   }
 
   render() {
@@ -46,15 +67,99 @@ export default class SingleView extends Component {
           )}
           {loadedDoc.document && (
             <Box>
-              <Heading>{loadedDoc.document.title}</Heading>
-              <Box>
-                <Text>
-                  created at{" "}
-                  <Moment format="DD/MM/YYYY">
-                    {loadedDoc.document.createdAt}
-                  </Moment>
-                </Text>
-              </Box>
+              <Heading>
+                <ClickToEdit
+                  value={this.state.title}
+                  endEditing={e => {
+                    this.setState({ title: e });
+                  }}
+                />
+              </Heading>
+              <Grid
+                rows={["full"]}
+                columns={["2/3", "1/3"]}
+                gap="small"
+                areas={[
+                  { name: "img_show", start: [0, 0], end: [0, 0] },
+                  { name: "info", start: [1, 0], end: [1, 0] }
+                ]}
+              >
+                <Box gridArea="img_show">Show OCR in here</Box>
+                <Box gridArea="info" background="light-5">
+                  <Box pad={{ bottom: "5px" }}>
+                    <Table>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell>Created</TableCell>
+                          <TableCell>
+                            <Moment format="DD.MM.YYYY">
+                              {loadedDoc.document.dated}
+                            </Moment>
+                          </TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Uploaded</TableCell>
+                          <TableCell>
+                            <Moment format="DD.MM.YYYY">
+                              {loadedDoc.document.createdAt}
+                            </Moment>
+                          </TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Owner</TableCell>
+                          <TableCell>
+                            {loadedDoc.document.owner.settings.displayName}
+                          </TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </Box>
+                  <Menu
+                    dropProps={{
+                      align: { top: "bottom", left: "left" }
+                    }}
+                    label="Actions"
+                    items={[
+                      {
+                        label: <Box alignSelf="center">Download</Box>,
+                        icon: (
+                          <Box pad="small">
+                            <Download />
+                          </Box>
+                        ),
+                        onClick: () => {}
+                      },
+                      {
+                        label: <Box alignSelf="center">Share</Box>,
+                        icon: (
+                          <Box pad="small">
+                            <Share />
+                          </Box>
+                        ),
+                        onClick: () => {}
+                      },
+                      {
+                        label: <Box alignSelf="center">Check In</Box>,
+                        icon: (
+                          <Box pad="small">
+                            <Upload />
+                          </Box>
+                        ),
+                        onClick: () => {}
+                      },
+                      {
+                        label: <Box alignSelf="center">Delete</Box>,
+                        icon: (
+                          <Box pad="small">
+                            <Trash />
+                          </Box>
+                        ),
+                        onClick: this.deleteFile
+                      }
+                    ]}
+                  />
+                </Box>
+              </Grid>
             </Box>
           )}
         </Box>
@@ -69,4 +174,5 @@ function mapState(state) {
 }
 
 const connectedSingleView = connect(mapState)(SingleView);
-export { connectedSingleView as SingleView };
+const routedSingle = withRouter(connectedSingleView);
+export { routedSingle as SingleView };
