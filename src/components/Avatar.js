da9b10030904bed788bd49920a4d17b4029e970b
Felix Wieland <mail@felixwie.com>
2020-03-29 16:04:51 +0200

    set working width and height

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -5,8 +5,8 @@ import { Box } from "grommet";
 export const Avatar = ({ name, url, ...rest }) => (
   <Box
     a11yTitle={`${name} avatar`}
-    height="avatar"
-    width="avatar"
+    height="64px"
+    width="64px"
     round="full"
     background={url ? `url(data:image/png;base64,${url})` : { color: "brand" }}
     {...rest}
