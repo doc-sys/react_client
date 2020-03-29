faec5880253bdc1e1d416d73bab3d75859dc5dde
Felix Wieland <mail@felixwie.com>
2020-03-29 19:49:19 +0200

    added user menu func

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -4,6 +4,8 @@ import { Menu, Text } from "grommet";
 
 import { Avatar } from "./Avatar";
 
+import { userService } from "../redux/_services/user.service";
+
 export const UserMenu = ({ user = {}, items = [], ...rest }) => (
   <Menu
     dropAlign={{ bottom: "top" }}
@@ -11,14 +13,26 @@ export const UserMenu = ({ user = {}, items = [], ...rest }) => (
     items={items.map(item => ({
       ...item,
       label: <Text size="small">{item.label}</Text>,
-      onClick: () => {} // no-op
+      onClick: e => {
+        switch (e.target.innerHTML) {
+          case "Logout":
+            userService.logout();
+            break;
+
+          case "Plan":
+            break;
+
+          default:
+            break;
+        }
+      } // no-op
     }))}
     label={
       <Avatar
         /* name={user.settings ? user.settings.displayName : null} */
         url={user.avatar}
-        width={"32px"}
-        height={"32px"}
+        width={"48px"}
+        height={"48px"}
       />
     }
     {...rest}
