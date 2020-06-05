1db3cad2e2bdd54c4348be0ca18de7aa09fc2c2c
Felix Wieland <mail@felixwie.com>
2020-06-05 11:31:53 +0200

    proxy
    added dev proxy; will redirect api and ws calls to dev core

---
+++ src/setupProxy.js
@@ -0,0 +1,25 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = function (app) {
+	// strip /api from api calls and route to dev core server
+	app.use(
+		'/api',
+		createProxyMiddleware({
+			target: 'http://localhost:3001',
+			changeOrigin: true,
+			pathRewrite: {
+				'^/api/': '',
+			},
+		})
+	)
+
+	// route ws calls to dev core server
+	app.use(
+		'/socket.io',
+		createProxyMiddleware({
+			target: 'http://localhost:3001',
+			changeOrigin: true,
+			ws: true,
+		})
+	)
+}
